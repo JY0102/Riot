1beed85a05e37bd665173303fce39947140252b3
jy4685 <jy468@DESKTOP-7AAHEAC>
2025-05-04 23:52:08 +0900

    롤 전적검색

--- 전적검색/전적검색/Converters/Converter.cs
+++ 전적검색/전적검색/Converters/Converter.cs
@@ -5,26 +5,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Riot;
+using 전적검색;
 
 namespace 전적검색
 {
     internal class InfoConverter : IValueConverter
     {
-        private static int idx = 0;
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            string key = (string)parameter;
 
-            var match = (Match)value;
-
-            if (match == null)
-            {
-                return "데이터없음";
-            }
-
-            return match.Participants[idx++][key];
+            return value;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -32,4 +25,208 @@ namespace 전적검색
             return null;
         }
     }
+}
+
+namespace 전적검색.Controls
+{
+    internal class ChampionConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string champion && !string.IsNullOrEmpty(champion))
+                return MainViewModel.Singletone.Dragon.GetChampionUrl(champion);
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class SpellConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string spell && !string.IsNullOrEmpty(spell))
+            {
+                string spell_name = MainViewModel.Singletone.DB.GetSpellName(spell);
+                return MainViewModel.Singletone.Dragon.GetSpellUrl(spell_name);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class RuneConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string Runes && !string.IsNullOrEmpty(Runes))
+            {
+                string[] rune = Runes.Split(':');
+                string rune_num = string.Empty;
+                if (rune.Length == 6)
+                {
+                    rune_num = rune[1];
+                }
+                else
+                {
+                    rune_num = rune[0];
+                }
+
+                return MainViewModel.Singletone.DB.GetRuneUrl(rune_num);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class ItemConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string Item && !string.IsNullOrEmpty(Item))
+            {
+                if (Item == "0")
+                    return "https://ddragon.leagueoflegends.com/cdn/5.5.1/img/ui/items.png";
+
+                return MainViewModel.Singletone.Dragon.GetItemUrl(Item);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class TimeConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime time)
+            {
+                TimeSpan diff = DateTime.Now - time;
+
+                if( diff.TotalDays < 1)
+                {
+                    if(diff.TotalHours < 1)
+                    {
+                        return $"{diff.TotalMinutes.ToString("N0")} 분 전";
+                    }
+
+                    return $"{diff.TotalHours.ToString("N0")} 시간 전";
+                }
+
+                return $"{diff.TotalDays.ToString("N0")} 일 전";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class WinConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+           if(value is bool win)
+            {
+                if (win)
+                {
+                    var color = (Color)ColorConverter.ConvertFromString("#FFFFEEEE");
+                    return new SolidColorBrush(color);
+
+                }
+                else
+                {
+                    var color = (Color)ColorConverter.ConvertFromString("#FFD4E4FE");
+                    return new SolidColorBrush(color);
+                }
+            }
+
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class WinToStringConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+           if(value is bool win)
+            {
+                if (win)
+                   return "승리";
+                else
+                    return "패배";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class KdaConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double Kda;
+
+           if(double.TryParse(value.ToString() ,out Kda))
+           {
+                if(Kda < 2.0)
+                {
+                    return Brushes.LightGray;
+                }
+                else if (Kda < 3.0)
+                {
+                    return Brushes.Green;
+                }
+                else if (Kda < 4.0)
+                {
+                    return Brushes.Blue;
+                }
+                else
+                {
+                    return Brushes.Red;
+                }
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
 }
---
+++ 전적검색/전적검색/DB/Database.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 전적검색
+{
+    internal class Database
+    {
+        private const string server_name = "DESKTOP-7AAHEAC";
+        private const string db_name = "Riot";
+        private const string sql_id = "home";
+        private const string sql_pw = "jy3685";
+
+        public DataSet ds { get; private set; } = new DataSet();
+        private SqlDataAdapter RuneAdapter { get; set; } = new SqlDataAdapter();
+        private SqlDataAdapter SpellAdapter { get; set; } = new SqlDataAdapter();
+
+        public DataTable RuneTable { get => ds.Tables["rune"]; }
+        public DataTable SpellTable { get => ds.Tables["spell"]; }
+
+        public SqlConnection scon = null;
+
+        public Database()
+        {
+            string con = string.Format($"Data Source={server_name};Initial Catalog={db_name};User ID={sql_id};Password={sql_pw}");
+            scon = new SqlConnection(con);
+
+            RuneAdapter.SelectCommand = RuneSelectAll();
+            RuneAdapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            RuneAdapter.Fill(ds, "rune");
+
+            SpellAdapter.SelectCommand = SpellSelectAll();
+            SpellAdapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            SpellAdapter.Fill(ds, "spell");
+
+            RuneAdapter.InsertCommand = RuneInsert();
+            RuneAdapter.DeleteCommand = RuneDelete();
+
+            SpellAdapter.InsertCommand = SpellInsert();
+            SpellAdapter.DeleteCommand = SpellDelete();
+        }
+
+        #region 룬 커맨드
+
+        private SqlCommand RuneSelectAll()
+        {
+            string sql = "select * from rune";
+            SqlCommand comm = new SqlCommand(sql, scon);
+            return comm;
+        }
+        private SqlCommand RuneInsert()
+        {
+            string sql = "insert into rune values(@perk_id, @perk_icon, @perk_kor_name);";
+            SqlCommand comm = new SqlCommand(sql, scon);
+            comm.Parameters.Add("@perk_id", SqlDbType.Int, sizeof(int), "perk_id");
+            comm.Parameters.Add("@perk_icon", SqlDbType.VarChar, 150, "perk_icon");
+            comm.Parameters.Add("@perk_kor_name", SqlDbType.VarChar, 50, "perk_kor_name");
+            return comm;
+        }
+        private SqlCommand RuneDelete()
+        {
+            string sql = "Delete from rune where perk_id = @perk_id;";
+            SqlCommand comm = new SqlCommand(sql, scon);
+
+            comm.Parameters.Add("@perk_id", SqlDbType.Int, 4, "perk_id");
+
+            return comm;
+        }
+        #endregion
+
+        #region 스펠 커맨드
+
+        private SqlCommand SpellSelectAll()
+        {
+            string sql = "select * from Spell";
+            SqlCommand comm = new SqlCommand(sql, scon);
+            return comm;
+        }
+        private SqlCommand SpellInsert()
+        {
+            string sql = "insert into Spell values(@Spell_id, @Spell_name, @Spell_kor_name);";
+            SqlCommand comm = new SqlCommand(sql, scon);
+            comm.Parameters.Add("@Spell_id", SqlDbType.Int, sizeof(int), "Spell_id");
+            comm.Parameters.Add("@Spell_name", SqlDbType.VarChar, 50, "Spell_name");
+            comm.Parameters.Add("@Spell_kor_name", SqlDbType.VarChar, 50, "Spell_kor_name");
+            return comm;
+        }
+        private SqlCommand SpellDelete()
+        {
+            string sql = "Delete from Spell where Spell_id = @Spell_id;";
+            SqlCommand comm = new SqlCommand(sql, scon);
+
+            comm.Parameters.Add("@Spell_id", SqlDbType.Int, 4, "Spell_id");
+
+            return comm;
+        }
+        #endregion
+
+
+        public void UpdateAll()
+        {
+            RuneAdapter.Update(RuneTable);
+            SpellAdapter.Update(SpellTable);
+        }
+
+        public string GetRuneUrl(string rune_num)
+        {
+            int perk_id = int.Parse(rune_num);
+
+            DataRow dr = RuneTable.Rows.Find(perk_id);
+
+            return dr["perk_icon"].ToString();
+        }
+
+        public string GetSpellName(string spell_num)
+        {
+            int spell_id = int.Parse(spell_num);
+
+            DataRow dr = SpellTable.Rows.Find(spell_id);
+
+            return dr["spell_name"].ToString();
+        }
+
+    }
+}
